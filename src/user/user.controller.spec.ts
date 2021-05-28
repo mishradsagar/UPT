82d71a526b0785887a71fe11e48e54dd518bbeff
Sagar Mishra <smishra@deqode.com>
2021-05-28 21:06:45 +0530

    feat(tests): added tests for user service methods

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,12 +1,21 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { rootMongooseTestModule } from '../../test/utils/mongoose.module';
 import { UserController } from './user.controller';
+import { User, UserSchema } from './user.model';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+      ],
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
